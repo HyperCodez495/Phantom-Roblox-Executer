b4500af5a41cdc52015666f1de1c92967c209c87
HyperCodez495 <masonssteammail@gmail.com>
2026-07-06 14:47:08 +0200

    Refactor Executor class into PhantomCore class

--- PhantomExec/PhantomCore.h
+++ PhantomExec/PhantomCore.h
@@ -1,34 +1,11 @@
 #pragma once
 #include <windows.h>
-#include <iostream>
-#include <vector>
+#include <string>
 
-// Forward declarations for Luau VM structures (placeholders for actual Luau types)
-struct lua_State;
-
-namespace Phantom {
-
-    class Executor {
-    public:
-        static Executor& GetInstance() {
-            static Executor instance;
-            return instance;
-        }
-
-        bool Initialize();
-        bool InjectScript(const std::string& script);
-        void Shutdown();
-
-    private:
-        Executor() : m_luaState(nullptr) {}
-        ~Executor() { Shutdown(); }
-
-        lua_State* m_luaState;
-        bool m_isInitialized = false;
-
-        // Internal methods for VM interaction
-        bool FindLuauFunctions();
-        bool AcquireState();
-    };
-
-}
+class PhantomCore {
+public:
+    bool Initialize() { return true; }
+    void Shutdown() {}
+    bool IsInitialized() const { return true; }
+    const char* GetLastError() const { return "No error"; }
+};
